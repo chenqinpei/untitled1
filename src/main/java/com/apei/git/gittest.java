92f2a185924ed9e50b44b366f91076222a0854be
cqinpei <1150196415@qq.com>
2022-07-21 23:08:26 +0800

    apei commit second

--- src/main/java/com/apei/git/gittest.java
+++ src/main/java/com/apei/git/gittest.java
@@ -3,5 +3,6 @@ package com.apei.git;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hellow git");
+        System.out.println("hellow git1");
     }
 }
